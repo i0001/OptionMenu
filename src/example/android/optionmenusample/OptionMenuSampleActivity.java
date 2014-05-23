8109a5b63aab010abf3264194a6ba7fe80add8ba
i0001 <il0venature1008@gmail.com>
2014-05-23 11:49:46 +0900

    Activityクラス修正

--- src/example/android/optionmenusample/OptionMenuSampleActivity.java
+++ src/example/android/optionmenusample/OptionMenuSampleActivity.java
@@ -2,7 +2,10 @@ package example.android.optionmenusample;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class OptionMenuSampleActivity extends Activity {
 
@@ -12,6 +15,9 @@ public class OptionMenuSampleActivity extends Activity {
 		setContentView(R.layout.activity_option_menu_sample);
 	}
 
+
+
+	// onCreateOptionsMenuメソッド(オプションメニュー生成)
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -19,4 +25,38 @@ public class OptionMenuSampleActivity extends Activity {
 		return true;
 	}
 
-}
+		// onOptionsItemSelectedメソッド(メニューアイテム選択処理)
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			switch (item.getItemId()) {
+				case R.id.item1:
+					showDialog("メニューアイテム1を選択しました。");
+					return true;
+				case R.id.item2:
+					showDialog("メニューアイテム2を選択しました。");
+					return true;
+				case R.id.item3:
+					showDialog("メニューアイテム3を選択しました。");
+					return true;
+				default:
+					return super.onOptionsItemSelected(item);
+			}
+		}
+
+		// showDialogメソッド(ダイアログ表示)
+		private void showDialog(String text) {
+			AlertDialog.Builder dialog
+					=new AlertDialog.Builder(OptionMenuSampleActivity.this);
+			dialog.setTitle("メニューアイテム選択結果");
+			dialog.setMessage(text);
+			dialog.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int whichButton) {
+					OptionMenuSampleActivity.this.setResult(Activity.RESULT_OK);
+				}
+			});
+			dialog.create();
+			dialog.show();
+		}
+
+	}
+
